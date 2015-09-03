137f84091819bb8302d74b743df2b2c842ef54f0
viettrung <dg.viettrung@gmail.com>
2015-09-03 23:13:13 +0700

    create test.py

--- hmm.py
+++ hmm.py
@@ -6,6 +6,7 @@ BROWN_CORPUS_DIR = 'brown'
 DICTIONARY_DIR = 'dictionary'
 TEST_DIR = 'test'
 
+WORD = 'word'
 WORD_TAG = 'word_tag'
 UNIGRAM = 'unigram'
 BIGRAM = 'bigram'
@@ -16,6 +17,7 @@ FILE_TEST = 'test'
 
 
 class BrownCorpus:
+    word_dict = {}
     word_tag_dict = {}
     unigram_tag_dict = {}
     bigram_tag_dict = {}
@@ -28,6 +30,7 @@ class BrownCorpus:
     def __init__(self):
 
         if os.path.isdir(DICTIONARY_DIR):
+            self.word_dict = get_trained_data(WORD)
             self.word_tag_dict = get_trained_data(WORD_TAG)
             self.unigram_tag_dict = get_trained_data(UNIGRAM)
             self.bigram_tag_dict = get_trained_data(BIGRAM)
@@ -49,6 +52,12 @@ class BrownCorpus:
                                     word, tag = word_tag.rsplit('/', 1)
 
                                     if count_file <= 490:
+
+                                        if word in self.word_dict:
+                                            self.word_dict[word] += 1
+                                        else:
+                                            self.word_dict[word] = 1
+
                                         if (word, tag) in self.word_tag_dict:
                                             self.word_tag_dict[word, tag] += 1
                                         else:
@@ -80,6 +89,7 @@ class BrownCorpus:
                         corpus_file.close()
 
             os.makedirs(DICTIONARY_DIR)
+            save_trained_data(self.word_dict, WORD)
             save_trained_data(self.word_tag_dict, WORD_TAG)
             save_trained_data(self.unigram_tag_dict, UNIGRAM)
             save_trained_data(self.bigram_tag_dict, BIGRAM)
@@ -99,8 +109,7 @@ class BrownCorpus:
 
     def get_q(self, penult_tag, last_tag, current_tag):
         if (penult_tag, last_tag, current_tag) in self.trigram_tag_dict:
-            return float(self.trigram_tag_dict[penult_tag, last_tag, current_tag]) / self.bigram_tag_dict[
-                last_tag, current_tag]
+            return float(self.trigram_tag_dict[penult_tag, last_tag, current_tag]) / self.bigram_tag_dict[last_tag, current_tag]
         else:
             return 0.0
 
@@ -111,22 +120,21 @@ class BrownCorpus:
             return sentence[k]
 
     def get_tag_sequence(self, sentence):
+        print('start tagging...')
         pi = {}
         pi[0, '', ''] = 1
         bp = {}
         n = len(sentence)
         y = {}
         for k in range(1, n + 1):
-            print('k: ', k)
             word = self.get_word(sentence, k - 1)
+            if word not in self.word_dict:
+                print("\033[93m <Warning: '%s' is not exist in the training data> \033[0m" % word)
             for u in self.get_tags(k - 1):
                 for v in self.get_tags(k):
-                    pi[k, u, v], bp[k, u, v] = max(
-                        [(pi[k - 1, w, u] * self.get_q(w, u, v) * self.get_e(word, v), w) for w in
-                         self.get_tags(k - 2)])
+                    pi[k, u, v], bp[k, u, v] = max([(pi[k - 1, w, u] * self.get_q(w, u, v) * self.get_e(word, v), w) for w in self.get_tags(k - 2)])
 
-        prob, y[n - 1], y[n] = max(
-            [(pi[n, u, v] * self.get_q(u, v, 'STOP'), u, v) for u in self.distinct_tags for v in self.distinct_tags])
+        prob, y[n - 1], y[n] = max([(pi[n, u, v] * self.get_q(u, v, 'STOP'), u, v) for u in self.distinct_tags for v in self.distinct_tags])
 
         for k in range(n - 2, 0, -1):
             y[k] = bp[k + 2, y[k + 1], y[k + 2]]
@@ -172,7 +180,7 @@ class BrownCorpus:
 
         finish_time = time.time()
         fout.close()
-        print('elapsed time tag sequence:', finish_time - start_time)
+        print('time to execute test_tag_sequence method:', finish_time - start_time)
 
 
 def save_trained_data(data, filename):
