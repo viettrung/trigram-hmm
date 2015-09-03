ef9920fe9a412ac23f809a969b6d6ee0fbbc91c5
viettrung <dg.viettrung@gmail.com>
2015-09-03 21:51:29 +0700

    correct syntax for python 3

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ s = time.time()
 
 hmm = BrownCorpus()
 
-# sentence = "Hello world. My name is luu."
+# It/pps was/bedz defeated/vbn in/in Congress/np last/ap year/nn ./.
+sentence = "It was defeated in Congress last year ."
 
-# y = hmm.get_tag_sequence(sentence.split())
-hmm.test_tag_sequence('test','test_result')
-hmm.test_accuracy('test_result');
+y = hmm.get_tag_sequence(sentence.split())
+# hmm.test_tag_sequence('test', 'test_result')
+# hmm.test_accuracy('test_result');
 
-# print(y)
+print(y)
 
 e = time.time()
 
